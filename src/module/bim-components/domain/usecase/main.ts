70fc5e729c02178d48adf6519aee4afaff793444
miguel gutierrez torres <miguel@tailorbird.us>
2024-02-22 15:56:23 -0500

    ifc per page x3

--- src/module/bim-components/domain/usecase/main.ts
+++ src/module/bim-components/domain/usecase/main.ts
@@ -125,7 +125,7 @@ window.addEventListener(
       let url = window.location.href;
       const texts = url.split("/");
       url = texts[texts.length - 1];
-      url.replace(".aspx", "");
+      url = url.replace(".aspx", "");
 
       for (const item of dataArr) {
         const { Name, URL } = item;
